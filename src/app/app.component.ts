fac37c2f2c135d3bfe3a795bb4c14a7fba8d2edc
genka <tamani.coding@gmail.com>
2021-06-19 12:03:23 +0200

    formbuilder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +9,25 @@ import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators }
 export class AppComponent {
   title = 'angular-reactive-forms-example';
 
-  elements = [ { key: 'fire', value: 'Fire'}, { key: 'ice', value: 'Ice'}, { key: 'water', value: 'Water'}, { key: 'thunder', value: 'Thunder'} ]
+  elements = [{ key: 'fire', value: 'Fire' }, { key: 'ice', value: 'Ice' }, { key: 'water', value: 'Water' }, { key: 'thunder', value: 'Thunder' }]
 
-  magicianForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(30), Validators.pattern('^[A-Za-z \-]*$')]),
-    birthday: new FormControl('', [Validators.required, this.atLeastTwenty]),
-    element: new FormControl('', [Validators.required]),
-    intelligence: new FormControl(60),
-    wisdom: new FormControl(60)
-  }, [this.validAttributes])
+  magicianForm: FormGroup
+
+  constructor(private formBuilder: FormBuilder) {
+    this.magicianForm = this.formBuilder.group({
+      name: formBuilder.control('', [Validators.required, Validators.minLength(5), Validators.maxLength(30), Validators.pattern('^[A-Za-z \-]*$')]),
+      birthday: formBuilder.control('', [Validators.required, this.atLeastTwenty]),
+      element: formBuilder.control('', [Validators.required]),
+      intelligence: formBuilder.control(60),
+      wisdom: formBuilder.control(60)
+    }, { validators: [this.validAttributes] });
+  }
 
   public onSubmit() {
     console.log(this.magicianForm.value)
   }
 
-  private atLeastTwenty (control: AbstractControl): ValidationErrors | null {
+  private atLeastTwenty(control: AbstractControl): ValidationErrors | null {
     if (control.value == null) {
       return null
     }
@@ -39,7 +43,7 @@ export class AppComponent {
     return null
   }
 
-  private validAttributes (control: AbstractControl): ValidationErrors | null {
+  private validAttributes(control: AbstractControl): ValidationErrors | null {
 
     if (control.get('element')?.value == 'fire' && control.get('intelligence')?.value < 60) {
       return { 'invalidFire': true }
