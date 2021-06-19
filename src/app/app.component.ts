bab0e6d74b964622272f0563a90a1ab2e19d7e00
genka <tamani.coding@gmail.com>
2021-06-19 11:57:04 +0200

    validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reactive-forms-example';
+
+  elements = [ { key: 'fire', value: 'Fire'}, { key: 'ice', value: 'Ice'}, { key: 'water', value: 'Water'}, { key: 'thunder', value: 'Thunder'} ]
+
+  magicianForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(30), Validators.pattern('^[A-Za-z \-]*$')]),
+    birthday: new FormControl('', [Validators.required, this.atLeastTwenty]),
+    element: new FormControl('', [Validators.required]),
+    intelligence: new FormControl(60),
+    wisdom: new FormControl(60)
+  }, [this.validAttributes])
+
+  public onSubmit() {
+    console.log(this.magicianForm.value)
+  }
+
+  private atLeastTwenty (control: AbstractControl): ValidationErrors | null {
+    if (control.value == null) {
+      return null
+    }
+
+    const birthday = new Date(control.value)
+    const birthdayYear = birthday.getFullYear()
+    const currentYear = new Date().getFullYear()
+
+    if (currentYear - birthdayYear < 20) {
+      return { 'invalidAge': true }
+    }
+
+    return null
+  }
+
+  private validAttributes (control: AbstractControl): ValidationErrors | null {
+
+    if (control.get('element')?.value == 'fire' && control.get('intelligence')?.value < 60) {
+      return { 'invalidFire': true }
+    }
+
+    if (control.get('element')?.value == 'ice' && control.get('intelligence')?.value < 75) {
+      return { 'invalidIce': true }
+    }
+
+    if (control.get('element')?.value == 'water' && control.get('wisdom')?.value < 65) {
+      return { 'invalidWater': true }
+    }
+
+    if (control.get('element')?.value == 'thunder' && control.get('wisdom')?.value < 80) {
+      return { 'invalidThunder': true }
+    }
+
+    return null
+  }
 }
